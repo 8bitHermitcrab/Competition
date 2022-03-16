76b2a0278fb0d073b5d7858d1efc07ece57a55c7
8bithermitcrab <kij517@naver.com>
2022-03-16 14:25:58 +0900

    220316

---
+++ Jupyter_ipynb_py_파일동시생성.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+pip install jupytext
+
+# conda activate [가상환경 이름]
+conda install ipykernel
+
+python -m ipykernel install --user --name base
+# python -m ipykernel install --user --name [가상환경 이름]
+
+conda env list
+
+
